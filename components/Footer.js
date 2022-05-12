1fdb3ca736fd3404884a3c89f59379909edf0246
Umesh Raghav <umeshraghav55@gmail.com>
2022-05-12 10:08:07 +0530

    First Commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,9 @@
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
+      <footer className={styles.footer}></footer>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>SAPEXIM | Electric Equipment Supplier in Delhi NCR India</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Welcome to sap exim" />
+        <p>Website is under development</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
