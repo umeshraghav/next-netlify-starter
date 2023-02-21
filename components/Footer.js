23af562d76e55780abe1bbaab12339404281543f
Umesh Raghav <umeshraghav55@gmail.com>
2023-02-21 22:50:33 +0530

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,32 +9,19 @@ export default function Footer() {
             <div className="col-lg-3">
               <a
                 className="navbar-brand d-inline-flex align-items-center mb-lg-4 mb-3"
-                href="index.html"
+                href="/"
               >
-                <span className="text-primary flex-shrink-0 me-2">
-                  <svg
-                    version="1.1"
-                    width="35"
-                    height="32"
-                    viewBox="0 0 36 33"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M35.6,29c-1.1,3.4-5.4,4.4-7.9,1.9c-2.3-2.2-6.1-3.7-9.4-3.7c-3.1,0-7.5,1.8-10,4.1c-2.2,2-5.8,1.5-7.3-1.1c-1-1.8-1.2-4.1,0-6.2l0.6-1.1l0,0c0.6-0.7,4.4-5.2,12.5-5.7c0.5,1.8,2,3.1,3.9,3.1c2.2,0,4.1-1.9,4.1-4.2s-1.8-4.2-4.1-4.2c-2,0-3.6,1.4-4,3.3H7.7c-0.8,0-1.3-0.9-0.9-1.6l5.6-9.8c2.5-4.5,8.8-4.5,11.3,0L35.1,24C36,25.7,36.1,27.5,35.6,29z"
-                    ></path>
-                  </svg>
-                </span>
+                <span className="text-primary flex-shrink-0 me-2"></span>
                 <span className="text-nav">SAP EXIM</span>
               </a>
               <p
                 className="mb-4 pb-lg-1 fs-xs text-muted"
                 style={{ maxWidth: "306px" }}
               >
-                The agency with extensive experience that creates marketing
-                strategies for B2B and B2C companies.
+                A Pioneer in providing fire, surveillance and security related
+                services
               </p>
-              <div className="mt-n3 ms-n3">
+              {/* <div className="mt-n3 ms-n3">
                 <a
                   className="btn btn-secondary btn-icon btn-sm btn-instagram rounded-circle mt-3 ms-3"
                   href="#"
@@ -53,7 +40,7 @@ export default function Footer() {
                 >
                   <i className="ai-linkedin"></i>
                 </a>
-              </div>
+              </div> */}
             </div>
             <div className="col-xl-8 offset-xl-1 col-lg-9">
               <div className="row row-cols-sm-4 row-cols-1">
@@ -107,43 +94,27 @@ export default function Footer() {
                 </div>
                 <div className="col">
                   <ul className="nav flex-column mb-0">
+                    <li className="nav-item mb-2">Address</li>
                     <li className="nav-item mb-2">
-                      <a className="nav-link p-0" href="#">
-                        About
-                      </a>
-                    </li>
-                    <li className="nav-item mb-2">
-                      <a className="nav-link p-0" href="#">
-                        Careers
-                      </a>
-                    </li>
-                    <li className="nav-item mb-2">
-                      <a className="nav-link p-0" href="#">
-                        News &amp; insights
-                      </a>
-                    </li>
-                    <li className="nav-item mb-2">
-                      <a className="nav-link p-0" href="#">
-                        Industry forecasts
-                      </a>
+                      Sector 3 Faridabad Haryana
                     </li>
                   </ul>
                 </div>
                 <div className="col">
                   <ul className="nav flex-column mb-0">
-                    <li className="nav-item mb-2">
+                    {/* <li className="nav-item mb-2">
                       <a className="nav-link p-0" href="mailto:+15262200444">
                         + 1 526 220 0444
                       </a>
-                    </li>
+                    </li> */}
                     <li className="nav-item mb-2">
-                      <a className="nav-link p-0" href="mailto:+15262200444">
-                        + 1 526 220 0000
+                      <a className="nav-link p-0" href="mailto:+919310234436">
+                        + 91 93102 34436
                       </a>
                     </li>
                     <li className="nav-item mb-2">
                       <a className="text-primary fw-semibold p-0" href="#">
-                        example@gmail.com
+                        info@sapexim.com
                       </a>
                     </li>
                   </ul>
@@ -151,17 +122,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className="fs-sm text-muted">
-            &copy; All rights reserved. Made by{" "}
-            <a
-              href="https://createx.studio/"
-              target="_blank"
-              rel="noopener"
-              className="text-dark text-decoration-none opacity-90"
-            >
-              Createx Studio
-            </a>
-          </div>
+          <div className="fs-sm text-muted">&copy; All rights reserved.</div>
         </div>
       </footer>
     </>
