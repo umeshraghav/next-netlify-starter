fb75e17e0e3c6240bbeff6112a5f17505c7b0b05
Umesh Raghav <umeshraghav55@gmail.com>
2023-02-21 10:53:59 +0530

    Added Hero Section

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -5,36 +5,16 @@ import Image from "next/image";
 function Hero() {
   return (
     <>
-      <div className="container col-xxl-8 px-4 py-5">
-        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6">
-            <Image
-              src={heroImage}
-              className="d-block mx-lg-auto img-fluid"
-              alt="Bootstrap Themes"
-              width="700"
-              height="500"
-              loading="lazy"
-            />
-          </div>
-          <div className="col-lg-6">
-            <h1 className="display-5 fw-bold lh-1 mb-3">
-              See everything, miss nothing - with our CCTV cameras
-            </h1>
-            <p className="lead">
-              leading supplier and service provider of top branded CCTV cameras
-              with installation and maintenance for CCTV systems, fire,
-              surveillance and security related services
-            </p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 me-md-2"
-              >
-                Contact Now
-              </button>
-            </div>
-          </div>
+      <div className="hero">
+        <div className="hero-banner">
+          <h1 className="hero-title">continue exploring</h1>
+          <p className="hero-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            explicabo debitis est autem dicta.
+          </p>
+          <a href="#featured" className="btn btn-white scroll-link">
+            explore tours
+          </a>
         </div>
       </div>
     </>
